42906ed57987788d79b19cf055dd73f7b41294bc
turhan-exe <65125305+turhan-exe@users.noreply.github.com>
2025-08-20 21:30:38 +0300

    chore: add vitest dependency

--- src/services/team.ts
+++ src/services/team.ts
@@ -1,5 +1,5 @@
 import { doc, setDoc, getDoc } from 'firebase/firestore';
-import { db } from '@/firebase';
+import { db } from '@/services/firebase';
 import { Player, ClubTeam } from '@/types';
 import { generateRandomName } from '@/lib/names';
 
